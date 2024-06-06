77339e323e41e5e8347da7ad427a75cb6a90e46f
Devashish Priyadarshi <devashish234073@gmail.com>
2024-06-06 09:23:09 +0530

    converted to maven project

---
+++ src/main/java/com/devashish/honest/social/net/honest_social_net/HonestSocialNetApplication.java
@@ -0,0 +1,13 @@
+package com.devashish.honest.social.net.honest_social_net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonestSocialNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonestSocialNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devashish/honest/social/net/honest_social_net/HonestSocialNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devashish.honest.social.net.honest_social_net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonestSocialNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
