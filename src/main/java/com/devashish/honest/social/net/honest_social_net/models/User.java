2b71573d0423b9fa79e70dc98e555722f9e8adcf
Devashish Priyadarshi <devashish234073@gmail.com>
2024-06-08 08:49:39 +0530

    added attributes for User object

--- src/main/java/com/devashish/honest/social/net/honest_social_net/models/User.java
+++ src/main/java/com/devashish/honest/social/net/honest_social_net/models/User.java
@@ -1,5 +1,41 @@
 package com.devashish.honest.social.net.honest_social_net.models;
 
-public class User {
+import java.util.List;
 
+public class User {
+    private String id;
+    private List<String> friendRequests;
+    private List<String> friends;
+    private List<String> friendsPosts;
+    private List<String> notifications;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public List<String> getFriendRequests() {
+		return friendRequests;
+	}
+	public void setFriendRequests(List<String> friendRequests) {
+		this.friendRequests = friendRequests;
+	}
+	public List<String> getFriends() {
+		return friends;
+	}
+	public void setFriends(List<String> friends) {
+		this.friends = friends;
+	}
+	public List<String> getFriendsPosts() {
+		return friendsPosts;
+	}
+	public void setFriendsPosts(List<String> friendsPosts) {
+		this.friendsPosts = friendsPosts;
+	}
+	public List<String> getNotifications() {
+		return notifications;
+	}
+	public void setNotifications(List<String> notifications) {
+		this.notifications = notifications;
+	}
 }
