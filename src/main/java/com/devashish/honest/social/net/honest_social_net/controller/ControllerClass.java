58c0a650e52fe0441999ea3f2ca6b4987b53b01c
Devashish Priyadarshi <devashish234073@gmail.com>
2024-06-09 10:52:04 +0530

    added code to inject logged in user to the model

--- src/main/java/com/devashish/honest/social/net/honest_social_net/controller/ControllerClass.java
+++ src/main/java/com/devashish/honest/social/net/honest_social_net/controller/ControllerClass.java
@@ -2,13 +2,22 @@ package com.devashish.honest.social.net.honest_social_net.controller;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.devashish.honest.social.net.honest_social_net.services.DaoService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Controller
 public class ControllerClass {
+	
+	@Autowired
+	private DaoService daoService;
+	
 	@GetMapping("/")
 	public String login(Model model) {
 		model.addAttribute("date", new Date());
@@ -17,7 +26,15 @@ public class ControllerClass {
 	
 	@GetMapping("/login")
 	public String login(@RequestParam("id") String id,Model model) {
-		model.addAttribute("date", id+" " + new Date());
+		var user = daoService.getUserById(id);
+		ObjectMapper objectMapper = new ObjectMapper();
+        String userJson;
+		try {
+			userJson = objectMapper.writeValueAsString(user);
+			model.addAttribute("user", userJson);
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
 		return "home";
 	}
 }
---
+++ src/main/java/com/devashish/honest/social/net/honest_social_net/controller/RestControllerClass.java
@@ -0,0 +1,8 @@
+package com.devashish.honest.social.net.honest_social_net.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestControllerClass {
+	
+}
