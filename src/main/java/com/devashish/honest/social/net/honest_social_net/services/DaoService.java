41bdb358d4cb297b9727439e1b04540bf801b4ea
Devashish Priyadarshi <devashish234073@gmail.com>
2024-06-10 08:58:10 +0530

    fixed null pointer on user not found

--- src/main/java/com/devashish/honest/social/net/honest_social_net/services/DaoService.java
+++ src/main/java/com/devashish/honest/social/net/honest_social_net/services/DaoService.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DaoService {
@@ -44,7 +45,8 @@ public class DaoService {
     }
     
     public LoggedInUser getUserById(String id) {
-        return userRepository.findById(id).get();
+    	Optional<LoggedInUser> user = userRepository.findById(id);
+        return user.isEmpty()?null:user.get();
     }
 
     public List<Comment> getAllComments() {
