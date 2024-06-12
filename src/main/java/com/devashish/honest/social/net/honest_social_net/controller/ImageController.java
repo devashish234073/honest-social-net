b6f690a8226dd27afdec9469f3786fcea3266abd
Devashish Priyadarshi <devashish234073@gmail.com>
2024-06-12 12:17:10 +0530

    partial implementation of getPosr

--- src/main/java/com/devashish/honest/social/net/honest_social_net/controller/ImageController.java
+++ src/main/java/com/devashish/honest/social/net/honest_social_net/controller/ImageController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -40,7 +41,7 @@ public class ImageController {
 	}
 	
 	@GetMapping("/getPost")
-	public byte[] getPost(String userId,String friendId,String postId) {
+	public byte[] getPost(@RequestParam("userId") String userId,@RequestParam("friendId") String friendId,@RequestParam("postId") String postId) {
 		Post post = daoService.getPostById(postId);
 		return post.getImage();
 	}
