c14f2a6a66ea108fb82ed32cc3327acf334f5c60
Devashish Priyadarshi <devashish234073@gmail.com>
2024-06-13 09:05:53 +0530

    fixed postStatus as it was not setting the postId to the user's post lst

--- src/main/java/com/devashish/honest/social/net/honest_social_net/controller/ImageController.java
+++ src/main/java/com/devashish/honest/social/net/honest_social_net/controller/ImageController.java
@@ -15,41 +15,45 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.devashish.honest.social.net.honest_social_net.models.LoggedInUser;
 import com.devashish.honest.social.net.honest_social_net.models.Post;
 import com.devashish.honest.social.net.honest_social_net.models.Post.VISIBILITY;
 import com.devashish.honest.social.net.honest_social_net.services.DaoService;
 
 @RestController
 public class ImageController {
-	
+
 	@Autowired
 	private DaoService daoService;
-	
+
 	@PostMapping("/postStatus")
-    public String postStatus(@RequestPart("image") MultipartFile image,
-    		@RequestPart("caption") String caption,
-            @RequestPart("userId") String userId) {
+	public String postStatus(@RequestPart("image") MultipartFile image, @RequestPart("caption") String caption,
+			@RequestPart("userId") String userId) {
+		LoggedInUser user = daoService.getUserById(userId);
 		Post post = new Post();
 		post.setCaption(caption);
 		post.setUserId(userId);
 		post.setVisibility(VISIBILITY.EVERYONE);
-		if(image!=null) {
+		if (image != null) {
 			try {
 				post.setImage(image.getBytes());
 			} catch (IOException e) {
 				e.printStackTrace();
-			}	
+			}
 		}
 		daoService.savePost(post);
+		user.addPost(post);
+		daoService.saveUser(user);
 		return "success";
 	}
-	
-	@GetMapping(value="/getPost")
-	public ResponseEntity<byte[]> getPost(@RequestParam("userId") String userId,@RequestParam("friendId") String friendId,@RequestParam("postId") String postId) {
+
+	@GetMapping(value = "/getPost")
+	public ResponseEntity<byte[]> getPost(@RequestParam("userId") String userId,
+			@RequestParam("friendId") String friendId, @RequestParam("postId") String postId) {
 		Post post = daoService.getPostById(postId);
 		HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_PNG);
-		return new ResponseEntity<>(post.getImage(),headers,HttpStatus.OK);
+		headers.setContentType(MediaType.IMAGE_JPEG);
+		return new ResponseEntity<>(post.getImage(), headers, HttpStatus.OK);
 	}
-    		
+
 }
